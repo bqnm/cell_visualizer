4a2b081671ec60cb27e3f60ed24370085e2c5e6a
Ben <ben@bqnm.com>
2021-08-12 11:14:34 -0400

    added removal of nan values in specified columns

--- cellvisualizer.py
+++ cellvisualizer.py
@@ -5,46 +5,75 @@ import matplotlib.pyplot as plt
 import scanpy as sc
 
 
-adata_selected = False
-column_selected = False
-
-#Set options
-st.set_option('deprecation.showPyplotGlobalUse', False)
-
-st.title('Cell Visualizer')
-
-
-uploaded_file = st.file_uploader(label='Upload .h5ad File')
-
-if uploaded_file != None:
-    adata = sc.read_h5ad(uploaded_file)
-    adata_selected = True
-    adata.obs.loc[:,adata.obs.isnull().sum() > 0]
-
-if adata_selected:
+#Replace stupid fucking NaN values with 'Unknown'
+def replace_nan(column):
+    if adata.obs[column].dtype == 'category':
+        adata.obs[column] = adata.obs[column].cat.add_categories('Unknown')
+        adata.obs[column].fillna('Unknown', inplace =True)
+         
+def add_filter():
+    #Select which column
     column_selectbox = st.sidebar.selectbox(
-        'Column',
+        'Column ',
         adata.obs.columns
     )
-
     
-    filter_check = st.sidebar.checkbox(label='Filter by category', )
 
+    #Replace NaN within column to stop errors
+    replace_nan(column_selectbox)
+
+    #Select to filter by category
+    filter_check = st.sidebar.checkbox(label='Filter by category ')
+    
+
+    #If filtering by category, choose which category
     if filter_check:
         column_values = adata.obs[column_selectbox].tolist()
         column_values = set(column_values)
         filter_selectbox = st.sidebar.selectbox(
-            'Filter',
+            'Filter ',
             column_values
     )
+        
 
+        #New adata based on filter
         filter_adata = adata[adata.obs[column_selectbox] == filter_selectbox].copy()
+      
 
+        #Filtered umap
         sc.pl.umap(filter_adata, color=[column_selectbox])
 
+
     else:
+        #Normal umap
+        filter_adata = adata
         sc.pl.umap(adata, color=[column_selectbox])
 
+    
+adata_selected = False
+
+
+#Set options
+st.set_option('deprecation.showPyplotGlobalUse', False)
+st.title('Cell Visualizer')
+
+
+#Upload .h5ad
+uploaded_file = st.file_uploader(label='Upload .h5ad File')
+
+
+#Check if file was uploaded
+if uploaded_file != None:
+    original_adata = sc.read_h5ad(uploaded_file)
+    adata = original_adata.copy()
+    adata_selected = True
+
+
+if adata_selected:
+    add_filter()
+    
+    #Display umap
     st.pyplot()
+
 
 
