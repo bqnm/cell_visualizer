5a8abc366311f82ba9cf5383a7a11c8463736bd6
Ben <ben@bqnm.com>
2021-08-20 13:06:53 -0400

    added choice for multiple maps and selections, added selections based on range, genes are now able to be selected and filtered, added color option

--- cellvisualizer.py
+++ cellvisualizer.py
@@ -6,15 +6,15 @@ import matplotlib.pyplot as plt
 import scanpy as sc
 
 
-st.set_page_config(layout="wide")
 
-col1, col2, col3 = st.columns((0.75,2,1.25))
+st.set_page_config(layout="wide")               
+col1, col2, col3 = st.columns((0.75,2,1))
 
 
 class DataSelection:
     def __init__(self, selection_number, adata):
         self.selection_number = selection_number
-        self.adata = adata #.copy()
+        self.adata = adata
         self.session_state_index = f'filter_count_{str(selection_number)}'
         if 'filter_count' not in st.session_state:
             st.session_state.filter_count = {}
@@ -31,16 +31,12 @@ class DataSelection:
         else:
             return column
 
-    def add_filter(self, filter_count, st_column, key_prefix, adata):
-        #Select which column
+    def add_filter(self, filter_count, st_column, key_prefix, adata, filter_type):
         column_selectbox = st_column.selectbox(
             f'Column {filter_count}' ,
             self.filter_select_list,
             key=f'{key_prefix} Column {filter_count}'
-        )        
-
-        
-        #Choose which category / range
+            )
         if column_selectbox not in adata.var_names:
             column_series = self.replace_nan(adata.obs[column_selectbox])
             if not is_numeric_dtype(column_series):
@@ -50,48 +46,68 @@ class DataSelection:
                     column_values,
                     key=f'{key_prefix} Filter {filter_count}'
                 )
-                    
-
-                #New adata based on filter
-                filter_adata = adata[column_series == filter_selectbox] #.copy()
-        
-                return filter_adata
-            
+                
+                filter_series = column_series == filter_selectbox
+                filter_label = column_selectbox
+                return filter_series
             else:
                 min_v, max_v = float(column_series.min()), float(column_series.max())
                 filter_range = st_column.slider(
                     label='Filter',
                     min_value=min_v,
                     max_value=max_v,
-                    value=(min_v, max_v)
+                    value=(min_v, max_v),
+                    key=f'{key_prefix} Slider {filter_count}'
                 )
                 
-
-                filter_adata = adata[adata.obs[column_selectbox].between(filter_range[0], filter_range[1])]
-
-                return filter_adata
-
-    def create_filters(self):
-        col1.title(f'Data Selection {str(self.selection_number)}')
-    
-        gene_ex_check = col1.checkbox(label='Check for gene expression mode', value=False, key=f'gene_ex_check_{str(self.selection_number)}')
-
-        filter_data = self.adata
-        if gene_ex_check == False:
-            filter_count = col1.selectbox('Filter count', range(10), key=f'filter_count_box_{str(self.selection_number)}')
-            st.session_state.filter_count[self.session_state_index] = filter_count
-
-            for i in range(st.session_state.filter_count[self.session_state_index]):
-                filter_data = self.add_filter(i+1, col1, str(self.selection_number), filter_data)
-            
-            fig = sc.pl.umap(filter_data, show=False, return_fig=True)
-            #Display umap
-            col2.pyplot(fig)
+                filter_series = adata.obs[column_selectbox].between(filter_range[0], filter_range[1])
+                return filter_series
 
         else:
-            gene_filter_selectbox = col1.selectbox('Gene filter', self.adata.var_names, key=f'gene_filter_selectbox_{self.selection_number}')
-            fig = sc.pl.umap(filter_data, show=False, return_fig=True, color=[gene_filter_selectbox])
-            col2.pyplot(fig)
+            gene_vals = adata[:,column_selectbox].X
+            min_v, max_v = float(gene_vals.min()), float(gene_vals.max())
+            filter_range = st_column.slider(
+                    label='Filter',
+                    min_value=min_v,
+                    max_value=max_v,
+                    value=(min_v, max_v),
+                    key=f'{key_prefix} Slider {filter_count}'
+                )
+            filter_series = (gene_vals > filter_range[0]) & (gene_vals < filter_range[1])
+            
+            return filter_series
+
+    def add_color_filter(self, st_column, key_prefix):
+        column_selectbox = st_column.selectbox(
+            'Color column' ,
+            self.filter_select_list,
+            key=f'{key_prefix} Color Column'
+            )
+
+        return column_selectbox
+
+    def create_filters(self):
+        col3.title(f'Data Selection {str(self.selection_number)}')
+
+        filter_adata = self.adata
+        
+        filter_count = col3.selectbox('Filter count', range(10), key=f'filter_count_box_{str(self.selection_number)}')
+        st.session_state.filter_count[self.session_state_index] = filter_count
+
+        for i in range(st.session_state.filter_count[self.session_state_index]):
+            filter_series = self.add_filter(i+1, col3, str(self.selection_number), filter_adata, 'selection')
+            filter_adata = filter_adata[filter_series]
+        
+        color_checkbox = col3.checkbox('Color map', key=f'color_checkbox_{str(self.selection_number)}')
+
+        if color_checkbox:
+            color_filter = self.add_color_filter(col3, str(self.selection_number))
+            fig = sc.pl.umap(filter_adata, show=False, return_fig=True, color=[color_filter])
+        
+        else:
+            fig = sc.pl.umap(filter_adata, show=False, return_fig=True)
+        
+        col2.pyplot(fig)
             
 data_selection_dict = {}
 
@@ -104,15 +120,12 @@ def create_data_selections():
 adata_selected = False
 
 
-#Set options
-col3.title('Cell Visualizer')
+col1.title('Cell Visualizer')
 
 
-#Upload .h5ad
-uploaded_file = col3.file_uploader(label='Upload .h5ad File')
+uploaded_file = col1.file_uploader(label='Upload .h5ad File')
 
 
-#Check if file was uploaded
 if uploaded_file != None:
     adata = sc.read_h5ad(uploaded_file)
     adata_selected = True
